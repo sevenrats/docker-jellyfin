0fb32edf370d756940d2ff85c9a83f2b72a34eec
sevenrats <sevvenrats@tutanota.com>
2022-12-31 10:10:07 -0500

    aarch64 and lsio migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,16 @@ JELLYFINDIR="/opt/jellyfin"
 FFMPEGDIR="/usr/share/jellyfin-ffmpeg"
 DATADIR="/data/jellyfin"
 
+  # support migration from lsio
+if [ -d "/config" ]; then
+  ln -sf /config/log $DATADIR/log
+  ln -sf /config/cache $DATADIR/cache
+  ln -sf /config/data $DATADIR/data
+  find /config -maxdepth 1 -not \
+    \( -wholename "/data" -o -wholename "/cache" -o -wholename "/log" -o -wholename "/config" \)\
+    -exec ln -sf {} $DATADIR/config \;
+fi
+
 # Launch App
   $JELLYFINDIR/jellyfin/jellyfin \
   -d $DATADIR/data \
